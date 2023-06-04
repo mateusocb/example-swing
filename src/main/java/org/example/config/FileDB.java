249564f540e360802488ea24cc9a925771f9c0b6
Mateus Oliveira <947444@lojasrenner.com.br>
2023-06-04 03:11:50 -0300

    create file if not exists

--- src/main/java/org/example/config/FileDB.java
+++ src/main/java/org/example/config/FileDB.java
@@ -4,6 +4,14 @@ import java.io.File;
 
 public class FileDB {
     public static File getFile() {
-        return new File("./data.csv");
+        File file = new File("./data.csv");
+
+        try {
+            file.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return file;
     }
 }
